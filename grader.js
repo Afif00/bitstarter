4e031718b86cf9592c0aacd217e8d225389a318b
Ubuntu <ubuntu@ip-172-31-19-44.us-west-2.compute.internal>
2013-08-28 05:16:57 +0000

    a try

--- grader.js
+++ grader.js
@@ -30,8 +30,8 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(url) {
     var resp ="";
-        rest.get(url).on('complete',function(response){
-          resp = response.toString();
+	rest.get(url).on('complete',function(response){
+	  resp = response.toString();
 	      if(resp =="") {
 		  console.log("%s does not exist. Exiting.", instr);
 		  process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
@@ -53,8 +53,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -67,10 +67,10 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
  //       .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <page_location>', 'location of page', clone(assertFileExists), HTMLFILE_DEFAULT) 
-        .parse(process.argv);
+	.option('-u, --file <page_location>', 'location of page', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
